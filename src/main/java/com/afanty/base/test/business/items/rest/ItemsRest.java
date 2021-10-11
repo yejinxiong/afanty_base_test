2f4cac364014a482a385700f79c847cfdf8bc6f1
叶金雄-中云-业务支撑 <yejx@uchonor.com>
2021-10-11 11:30:43 +0800

    swagger2优化map传参

--- src/main/java/com/afanty/base/test/business/items/rest/ItemsRest.java
+++ src/main/java/com/afanty/base/test/business/items/rest/ItemsRest.java
@@ -10,17 +10,33 @@ import com.afanty.base.test.common.web.ResponseResult;
 import com.afanty.base.test.common.web.StatusCode;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * <p>
@@ -48,63 +64,12 @@ public class ItemsRest {
     @Resource(name = "itemsServiceImpl")
     private ItemsServiceImpl itemsService;
 
-    @RequestMapping(value = "/remotegetlist", method = RequestMethod.GET)
-    public ResponseResult remoteGetList(@RequestParam Map<String, String> param) {
-        LOGGER.info("远程条件查询评分项表, 参数：{}", JSONObject.toJSONString(param));
-        ResponseResult rr = new ResponseResult();
-        try {
-            String url = "http://127.0.0.1:9090/mp/ac/system/items/getlist";
-            LOGGER.info("远程条件查询评分项表, url：{}", url);
-            String result = OkHttpUtil.get(url, param, null);
-            JSONObject responseObject = JSONObject.parseObject(result);
-            LOGGER.info("远程条件查询评分项表, 结果：{}", JSONObject.toJSONString(responseObject));
-            List<Items> itemsList = Optional.ofNullable(responseObject)
-                    .map(response -> JSONObject.parseArray(response.getString("data"), Items.class))
-                    .orElseGet(ArrayList::new);
-            rr.setData(itemsList);
-        } catch (Exception e) {
-            LOGGER.error("远程条件查询评分项表异常：{}", e.getMessage());
-            rr = new ResponseResult(MsgCode.FAILURE.getKey(), StatusCode.CODE_3000.getKey(), StatusCode.CODE_3000.getDesc(), null);
-        }
-        return rr;
-    }
-
-    @Transactional(rollbackFor = Exception.class)
-    @RequestMapping(value = "/remoteupdate", method = RequestMethod.POST)
-    public ResponseResult remoteUpdate(Items items) {
-        LOGGER.info("本地修改标签 参数：{}", JSONObject.toJSONString(items));
-        ResponseResult rr = new ResponseResult();
-        if (items == null || StringUtils.isEmpty(items.getItemsId())) {
-            rr.setStatus(StatusCode.CODE_1000.getKey());
-            rr.setMsg(StatusCode.CODE_1000.getDesc());
-            return rr;
-        }
-        try {
-            itemsService.updateById(items);
-            int i = 1/0;
-
-            Map<String, String> param = new HashMap<>();
-            param.put("itemsId", "01a4d6dd5ace4e1cbf56c31d3dd698a4");
-            param.put("field1", "测试1");
-            LOGGER.info("远程修改评标签，参数：{}", JSONObject.toJSONString(param));
-            String url = "http://127.0.0.1:9090/mp/ac/system/items/update";
-            LOGGER.info("远程修改评标签, url：{}", url);
-            String result = OkHttpUtil.post(url, param, null);
-            JSONObject responseObject = JSONObject.parseObject(result);
-            LOGGER.info("远程修改标签, 结果：{}", JSONObject.toJSONString(responseObject));
-        } catch (Exception e) {
-            LOGGER.error("修改标签异常：{}", e.getMessage());
-            rr = new ResponseResult(MsgCode.FAILURE.getKey(), StatusCode.CODE_3000.getKey(), StatusCode.CODE_3000.getDesc(), null);
-        }
-        return rr;
-    }
-
+    @ApiOperation(value = "条件查询评分项表", notes = "条件查询评分项表", response = ResponseResult.class)
     @ApiImplicitParams({
             @ApiImplicitParam(name = "itemsName", value = "标签名称", dataType = "String", paramType = "query"),
             @ApiImplicitParam(name = "itemsType", value = "标签类型", dataType = "Integer", paramType = "query")})
-    @ApiOperation(value = "条件查询评分项表", notes = "条件查询评分项表", response = ResponseResult.class)
     @RequestMapping(value = "/getlist", method = RequestMethod.GET)
-    public ResponseResult getlist(@RequestParam(required = false) Map<String, Object> param) {
+    public ResponseResult getlist(@RequestParam @ApiParam(hidden = true) Map<String, Object> param) {
         LOGGER.info("条件查询评分项表, 参数：{}", JSONObject.toJSONString(param));
         ResponseResult rr = new ResponseResult();
         try {
@@ -117,8 +82,11 @@ public class ItemsRest {
     }
 
     @ApiOperation(value = "分页查询评分项表", notes = "分页查询评分项表", response = ResponseResult.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "itemsName", value = "标签名称", dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "itemsType", value = "标签类型", dataType = "Integer", paramType = "query")})
     @RequestMapping(value = "/getpagelist", method = RequestMethod.GET)
-    public ResponseResult getpagelist(@RequestParam Map<String, Object> param) {
+    public ResponseResult getpagelist(@RequestParam @ApiParam(hidden = true) Map<String, Object> param) {
         LOGGER.info("条件查询评分项表, 参数：{}", JSONObject.toJSONString(param));
         ResponseResult rr = new ResponseResult();
         Page<Items> page = new Page<>();
@@ -137,9 +105,10 @@ public class ItemsRest {
         return rr;
     }
 
-    @ApiOperation(value = "根据id批量查询", notes = "将所有id放到一个字符串", response = ResponseResult.class)
+    @ApiOperation(value = "根据id批量查询", notes = "所有id以逗号分割", response = ResponseResult.class)
+    @ApiImplicitParams({@ApiImplicitParam(name = "itemsIds", value = "标签id逗号分割", dataType = "String", paramType = "query")})
     @RequestMapping(value = "/getlistbatchbyids", method = RequestMethod.GET)
-    public ResponseResult getlistbatchbyids(@RequestParam Map<String, Object> param) {
+    public ResponseResult getlistbatchbyids(@RequestParam @ApiParam(hidden = true) Map<String, Object> param) {
         LOGGER.info("根据id批量查询, 参数：{}", JSONObject.toJSONString(param));
         ResponseResult rr = new ResponseResult();
         String itemsIds = MapUtils.getString(param, "itemsIds");
@@ -159,7 +128,7 @@ public class ItemsRest {
         return rr;
     }
 
-    @ApiOperation(value = "新建", notes = "param：items", response = ResponseResult.class)
+    @ApiOperation(value = "新建", notes = "新建", response = ResponseResult.class)
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public ResponseResult save(Items items) {
         LOGGER.info("新建, 参数：{}", JSONObject.toJSONString(items));
@@ -181,7 +150,7 @@ public class ItemsRest {
         return rr;
     }
 
-    @ApiOperation(value = "批量新建", notes = "param：items", response = ResponseResult.class)
+    @ApiOperation(value = "批量新建", notes = "批量新建", response = ResponseResult.class)
     @RequestMapping(value = "/savebatch", method = RequestMethod.POST)
     public ResponseResult savebatch(@RequestBody List<Items> list) {
         LOGGER.info("批量新建, 参数：{}", JSONObject.toJSONString(list));
@@ -200,7 +169,7 @@ public class ItemsRest {
         return rr;
     }
 
-    @ApiOperation(value = "修改", notes = "param：items", response = ResponseResult.class)
+    @ApiOperation(value = "修改", notes = "修改", response = ResponseResult.class)
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public ResponseResult update(Items items) {
         LOGGER.info("修改, 参数：{}", JSONObject.toJSONString(items));
@@ -219,9 +188,12 @@ public class ItemsRest {
         return rr;
     }
 
-    @ApiOperation(value = "根据id批量修改使用状态", notes = "param：itemsIds,isUse", response = ResponseResult.class)
+    @ApiOperation(value = "根据id批量修改使用状态", notes = "根据id批量修改使用状态", response = ResponseResult.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "itemsIds", value = "标签id", dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "isUse", value = "使用状态", dataType = "Integer", paramType = "query")})
     @RequestMapping(value = "/updateusebatchbyid", method = RequestMethod.POST)
-    public ResponseResult updateUseBatchById(@RequestParam Map<String, Object> param) {
+    public ResponseResult updateUseBatchById(@RequestParam @ApiParam(hidden = true) Map<String, Object> param) {
         LOGGER.info("根据id批量修改使用状态, 参数：{}", JSONObject.toJSONString(param));
         ResponseResult rr = new ResponseResult();
         String itemsIds = MapUtils.getString(param, "itemsIds");
@@ -281,6 +253,62 @@ public class ItemsRest {
             itemsService.removeById(itemsId);
         } catch (Exception e) {
             LOGGER.error("根据id删除评分项错误：{}", e.getMessage());
+            rr = new ResponseResult(MsgCode.FAILURE.getKey(), StatusCode.CODE_3000.getKey(), StatusCode.CODE_3000.getDesc(), null);
+        }
+        return rr;
+    }
+
+    @ApiOperation(value = "远程条件查询评分项表", notes = "远程条件查询评分项表", response = ResponseResult.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "itemsName", value = "标签名称", dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "itemsType", value = "标签类型", dataType = "Integer", paramType = "query")})
+    @RequestMapping(value = "/remotegetlist", method = RequestMethod.GET)
+    public ResponseResult remoteGetList(@RequestParam @ApiParam(hidden = true) Map<String, String> param) {
+        LOGGER.info("远程条件查询评分项表, 参数：{}", JSONObject.toJSONString(param));
+        ResponseResult rr = new ResponseResult();
+        try {
+            String url = "http://127.0.0.1:9090/mp/ac/system/items/getlist";
+            LOGGER.info("远程条件查询评分项表, url：{}", url);
+            String result = OkHttpUtil.get(url, param, null);
+            JSONObject responseObject = JSONObject.parseObject(result);
+            LOGGER.info("远程条件查询评分项表, 结果：{}", JSONObject.toJSONString(responseObject));
+            List<Items> itemsList = Optional.ofNullable(responseObject)
+                    .map(response -> JSONObject.parseArray(response.getString("data"), Items.class))
+                    .orElseGet(ArrayList::new);
+            rr.setData(itemsList);
+        } catch (Exception e) {
+            LOGGER.error("远程条件查询评分项表异常：{}", e.getMessage());
+            rr = new ResponseResult(MsgCode.FAILURE.getKey(), StatusCode.CODE_3000.getKey(), StatusCode.CODE_3000.getDesc(), null);
+        }
+        return rr;
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @ApiOperation(value = "本地和远程修改标签", notes = "本地和远程修改标签", response = ResponseResult.class)
+    @RequestMapping(value = "/remoteupdate", method = RequestMethod.POST)
+    public ResponseResult remoteUpdate(Items items) {
+        LOGGER.info("本地和远程修改标签，参数：{}", JSONObject.toJSONString(items));
+        ResponseResult rr = new ResponseResult();
+        if (items == null || StringUtils.isEmpty(items.getItemsId())) {
+            rr.setStatus(StatusCode.CODE_1000.getKey());
+            rr.setMsg(StatusCode.CODE_1000.getDesc());
+            return rr;
+        }
+        try {
+            itemsService.updateById(items);
+            int i = 1/0;
+
+            Map<String, String> param = new HashMap<>();
+            param.put("itemsId", "01a4d6dd5ace4e1cbf56c31d3dd698a4");
+            param.put("field1", "测试1");
+            LOGGER.info("远程修改评标签，参数：{}", JSONObject.toJSONString(param));
+            String url = "http://127.0.0.1:9090/mp/ac/system/items/update";
+            LOGGER.info("远程修改评标签, url：{}", url);
+            String result = OkHttpUtil.post(url, param, null);
+            JSONObject responseObject = JSONObject.parseObject(result);
+            LOGGER.info("远程修改标签, 结果：{}", JSONObject.toJSONString(responseObject));
+        } catch (Exception e) {
+            LOGGER.error("本地和远程修改标签异常：{}", e.getMessage());
             rr = new ResponseResult(MsgCode.FAILURE.getKey(), StatusCode.CODE_3000.getKey(), StatusCode.CODE_3000.getDesc(), null);
         }
         return rr;
