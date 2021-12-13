e762b1baac827a2389ebf818afc2f690a36f065f
叶金雄 <yejinxiong01@126.com>
2021-12-13 20:45:02 +0800

    zookeeper3.7.0学习

--- src/main/java/com/afanty/base/test/AfantyBaseTestApplication.java
+++ src/main/java/com/afanty/base/test/AfantyBaseTestApplication.java
@@ -1,7 +1,8 @@
 package com.afanty.base.test;
 
-import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,14 +14,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @date 2021-05-20
  */
 @SpringBootApplication
-@Slf4j
 @EnableSwagger2
 @MapperScan(basePackages = "com.afanty.base.test")
 public class AfantyBaseTestApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AfantyBaseTestApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AfantyBaseTestApplication.class, args);
-        log.info("已启动");
+        LOGGER.info("已启动");
     }
 
 }
